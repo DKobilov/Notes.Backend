3d4215394435ba3ee72101ceb932905c5328ae7f
DilshodK0592 <k.dilshod@eskhata.tj>
2025-08-05 11:03:00 +0500

    added layers and projects

---
+++ Notes.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace Notes.Application.Common.Mappings
+{
+    public class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly)
+        {
+            ApplyMappingsFromAssembly(assembly);
+        }
+
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()//класс ищет все типы которые реализуют интерфейс imapwith<>
+                .Where(type => type.GetInterfaces()
+                    .Any(i => i.IsGenericType &&
+                    i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");//класс вызывает метод Mapping из типа или из интерфейса если в типе отсутствует
+                methodInfo?.Invoke(instance, [this]);
+            }
+        }
+    }
+}
